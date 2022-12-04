e75a8d25cd5a4782fcfd6661b8ae423723fe660c
GabrielGalatti <gabrielgalatti@hotmail.com>
2022-12-04 19:34:15 -0300

    feat: process title responsive

--- src/components/templates/Process.tsx
+++ src/components/templates/Process.tsx
@@ -1,4 +1,4 @@
-import { Box, Flex } from "@chakra-ui/react";
+import { Box, Flex, useBreakpointValue } from "@chakra-ui/react";
 
 import { COLORS } from "../../config/colors";
 
@@ -12,29 +12,38 @@ type ProcessProps = {
   bgPath: string;
 };
 
-const Process = ({ bgPath, processItems, title }: ProcessProps) => (
-  <Box pos="relative" pt="50px">
-    <Box
-      w="100"
-      bgColor={COLORS.PRIMARY}
-      py="25px"
-      display="flex"
-      justifyContent="center"
-    >
-      <StyledText
-        boldColor={COLORS.SECONDARY}
-        regularColor={COLORS.WHITE}
-        size="35px"
-        text={title}
-        textAlign="center"
-      />
+const Process = ({ bgPath, processItems, title }: ProcessProps) => {
+  const titleSize = useBreakpointValue({
+    base: "25px",
+    md: "30px",
+    lg: "35px",
+  }) as `${number}px`;
+
+  return (
+    <Box pos="relative" pt="50px">
+      <Box
+        w="100"
+        bgColor={COLORS.PRIMARY}
+        py="25px"
+        display="flex"
+        justifyContent="center"
+      >
+        <StyledText
+          boldColor={COLORS.SECONDARY}
+          regularColor={COLORS.WHITE}
+          size={titleSize}
+          text={title}
+          textAlign="center"
+          lineHeight={titleSize === "35px" ? undefined : "40px"}
+        />
+      </Box>
+      <Box w="100%" bgImage={bgPath} bgRepeat="repeat-y" bgSize="100%">
+        <Flex px="10%" pt="5%" pb="5%">
+          <ProcessFlow processItems={processItems} />
+        </Flex>
+      </Box>
     </Box>
-    <Box w="100%" bgImage={bgPath} bgRepeat="repeat-y" bgSize="100%">
-      <Flex px="10%" pt="5%" pb="5%">
-        <ProcessFlow processItems={processItems} />
-      </Flex>
-    </Box>
-  </Box>
-);
+  );
+};
 
 export default Process;
